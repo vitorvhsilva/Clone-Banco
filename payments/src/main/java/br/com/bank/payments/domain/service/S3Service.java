ed771f5d44f462d784bd9275e4b9c083c30bdb2d
vitorvhsilva <dndragonbr@gmail.com>
2025-03-04 16:22:56 -0300

    Lógica de salvar transações do usuário no S3 feita com sucesso

--- payments/src/main/java/br/com/bank/payments/domain/service/S3Service.java
+++ payments/src/main/java/br/com/bank/payments/domain/service/S3Service.java
@@ -2,20 +2,26 @@ package br.com.bank.payments.domain.service;
 
 import br.com.bank.payments.api.dto.events.TransacaoS3DTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class S3Service {
 
-    private S3Client s3Client;
-    private ObjectMapper objectMapper;
+    private final S3Client s3Client;
+    private final ObjectMapper objectMapper;
 
     public S3Service() {
         this.s3Client = S3Client.builder()
@@ -24,9 +30,18 @@ public class S3Service {
         this.objectMapper = new ObjectMapper();
     }
 
-    public void salvarTransacaoNoS3(String chave, TransacaoS3DTO data) {
+    public void salvarTransacaoNoS3(String chave, TransacaoS3DTO novaTransacao) {
+        chave += ".json"; // forcando para ser json
+
         try {
-            String jsonString = objectMapper.writeValueAsString(data);
+            List<TransacaoS3DTO> transacoes = recuperarListaTransacoesDoS3(chave);
+
+            if (transacoes == null) {
+                transacoes = new ArrayList<>();
+            }
+            transacoes.add(novaTransacao);
+
+            String jsonString = objectMapper.writeValueAsString(transacoes);
 
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket("ms-payments-bucket")
@@ -35,10 +50,38 @@ public class S3Service {
 
             s3Client.putObject(putObjectRequest, RequestBody.fromString(jsonString));
 
-            System.out.println("Deu certo!");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    private List<TransacaoS3DTO> recuperarListaTransacoesDoS3(String chave) {
+        try {
+            HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                    .bucket("ms-payments-bucket")
+                    .key(chave)
+                    .build();
+
+            s3Client.headObject(headObjectRequest); // lanca excecao se nao existir
+
+            // se existir recupera
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket("ms-payments-bucket")
+                    .key(chave)
+                    .build();
+
+            String jsonString = s3Client.getObjectAsBytes(getObjectRequest).asUtf8String();
+
+            // converte json para lista
+            return objectMapper.readValue(jsonString, new TypeReference<List<TransacaoS3DTO>>() {});
+
+        } catch (NoSuchKeyException e) {
+            // arquivo nao existe
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro!");
+            return null;
+        }
+    }
 }
+
